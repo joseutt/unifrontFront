4a5c21381162b0538b6d6261ad643be835e31d1a
LankyAngel <angelescalante1908@gmail.com>
2026-06-18 21:47:59 -0700

    debugging2

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import {
   markSessionExpired,
 } from "./session";
 
-console.log("API URL:", import.meta.env.VITE_API_URL);
+console.log("API URL probando:", import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
 });
